3854791a8c80a484c8dcc3ac8db1975b579e4318
Fyodor Simonov <fdr.smnv@outlook.com>
2021-11-24 17:27:47 +0300

    Refactor serer startup file

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,17 +1,40 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose, { ConnectOptions } from 'mongoose';
-import { userAPI } from './api/user.js';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import { userRoutes } from './routes/index.js';
 
 dotenv.config();
-const connection = mongoose.connection;
+const { PORT, MONGO_URI, MONGO_PORT, SESS_NAME, SESS_SECRET, SESS_LIFETIME } = process.env;
+const mongoUrl = `${MONGO_URI}${MONGO_PORT}`;
 
-connection.once("open", () => console.log("MongoDB database connection established successfully"));
+(async () => {
+    try {
+        await mongoose.connect(mongoUrl, { useUnifiedTopology: true, useNewUrlParser: true } as ConnectOptions);
+        console.log("MongoDB | connected");
 
-const app = express();
-const port = process.env.PORT;
+        const app = express();
+        app.disable('x-powered-by');
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use(userAPI);
+        app.use(session({
+            secret: SESS_SECRET as string,
+            resave: false,
+            saveUninitialized: true,
+            store: MongoStore.create({
+                mongoUrl,
+                ttl: 60 * 60 * 24, // 1 Day
+            })
+        }))
 
-app.listen(port, () => console.log(`server is running on port ${port}`));
+        const apiRouter = express.Router();
+        app.use('/api', apiRouter);
+        apiRouter.use('/user', userRoutes);
+
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    } catch (error) {
+        console.error(error)
+    }
+})()
