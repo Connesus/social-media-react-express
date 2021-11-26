685cec7af8e00556cce3f89c20c1482d5be74535
Fyodor Simonov <fdr.smnv@outlook.com>
2021-11-27 00:26:03 +0300

    Add user actions

--- src/client/src/redux/slice/user.ts
+++ src/client/src/redux/slice/user.ts
@@ -1,3 +1,4 @@
+import { SessionUserT } from './../../../../server/src/utils/helpers';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 export interface UserState {
@@ -16,6 +17,12 @@ export const counterSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
+        loginUser: (state, action) => { },
+        createUser: (state, action) => { },
+        getSessionData: () => { },
+        setSessionData: (state, action: PayloadAction<SessionUserT>) => {
+            return { ...state, ...action.payload }
+        },
         setUsername: (state, action) => {
             state.username = action.payload
         },
@@ -29,6 +36,6 @@ export const counterSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { setUserId, setUsername, setLoginStatus } = counterSlice.actions
+export const { setUserId, setUsername, setLoginStatus, createUser, loginUser, getSessionData, setSessionData } = counterSlice.actions
 
 export default counterSlice.reducer
