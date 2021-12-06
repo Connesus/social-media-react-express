225cad593be0f992cfccca95617865771f9413ec
Fyodor Simonov <fdr.smnv@outlook.com>
2021-12-06 13:36:10 +0300

    Update sessionize helper

--- src/server/src/utils/helpers.ts
+++ src/server/src/utils/helpers.ts
@@ -1,6 +1,7 @@
 
+import { ObjectId } from 'mongoose';
 import { IUser } from './../model/user';
 
-export type SessionUserT = { userId: string, username: string }
+export type SessionUserT = { id?: ObjectId, username?: string }
 
-export const sessionizeUser: (user: IUser) => SessionUserT = ({ id, username }: IUser) => ({ userId: id, username });
+export const sessionizeUser: (user: IUser) => SessionUserT = ({ id, username }) => ({ id, username });
