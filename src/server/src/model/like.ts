59f476fbaada9c3b786a499723b3b29f1ad9ce18
Fyodor Simonov <fdr.smnv@outlook.com>
2021-12-10 12:14:24 +0300

    Create Like model

---
+++ src/server/src/model/like.ts
@@ -0,0 +1,24 @@
+import { IUser, UserModel } from './user.js';
+import { IPost, Post } from './post.js';
+import mongoose, { Document } from "mongoose";
+const { Schema, model } = mongoose
+
+
+export interface ILike extends Document {
+    postId: IPost['id'];
+    userId: IUser['id'];
+}
+
+
+const likeSchema = new Schema<ILike>({
+    postId: { type: Schema.Types.ObjectId, required: true },
+    userId: { type: Schema.Types.ObjectId, required: true }
+})
+
+export const LikeModel = model<ILike>('Like', likeSchema);
+
+export class LikeService {
+    static async getByUserAndPostIds(userId: ILike['userId'], postId: ILike['postId']) {
+        return await LikeModel.findOne({ userId, postId });
+    }
+}
