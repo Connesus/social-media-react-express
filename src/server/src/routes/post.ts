329b279caf9b0e4b538b78da090b9d4686c263ca
Fyodor Simonov <fdr.smnv@outlook.com>
2021-12-13 22:51:31 +0300

    Add createPost to router

--- src/server/src/routes/post.ts
+++ src/server/src/routes/post.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import postController from "../controller/post.js";
+import multer from 'multer';
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage })
 
 const postRouter = Router();
 
 postRouter.post('/page', postController.getPage);
-postRouter.post('/like', postController.likePost)
+postRouter.post('/like', postController.likePost);
+postRouter.post('', upload.single('image'), postController.createPost);
 
 export default postRouter;
