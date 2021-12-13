5fb9ea24346e8b120bab1e2978b88fe144fe22ba
Fyodor Simonov <fdr.smnv@outlook.com>
2021-12-13 22:49:23 +0300

    Controller for post creation

--- src/server/src/controller/post.ts
+++ src/server/src/controller/post.ts
@@ -1,5 +1,10 @@
-import { RequestHandler } from "express"
+import { ImageModel } from '../model/image.js';
+import { RequestHandler, Request } from "express"
 import { PostService } from "../model/post.js";
+
+interface createPostRequest extends Request {
+    file: Express.Multer.File
+}
 
 const postController: { [key: string]: RequestHandler } = {
     getPage: async (req, res) => {
@@ -13,6 +18,19 @@ const postController: { [key: string]: RequestHandler } = {
         // PostService.likePost(req.session.user.id, postId);
         await PostService.likePost(postId, '61aa0e3e8a1f72f9df6ccf66');
         res.end();
+    },
+    // @ts-ignore
+    createPost: async (req: createPostRequest, res) => {
+        let imageId;
+        if (req.file) {
+            const newImage = new ImageModel({ data: req.file.buffer, type: req.file.mimetype, name: req.file.originalname });
+            await newImage.save();
+            imageId = newImage.id;
+        }
+        console.log(imageId ? `imageId: ${imageId}` : 'No image');
+        const postData = JSON.parse(JSON.parse(req.body.postData));
+        await PostService.createPost({ ...postData, imageId })
+        res.end()
     }
 }
 
