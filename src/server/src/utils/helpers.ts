3faa604f14bd2fbf85947f98db9d3081ddbff64d
Fyodor Simonov <fdr.smnv@outlook.com>
2021-12-23 12:21:11 +0300

    Update session user type

--- src/server/src/utils/helpers.ts
+++ src/server/src/utils/helpers.ts
@@ -2,6 +2,6 @@
 import { ObjectId } from 'mongoose';
 import { IUser } from './../model/user';
 
-export type SessionUserT = { id?: ObjectId, username?: string }
+export type SessionUserT = { id: string, username: string }
 
 export const sessionizeUser: (user: IUser) => SessionUserT = ({ id, username }) => ({ id, username });
