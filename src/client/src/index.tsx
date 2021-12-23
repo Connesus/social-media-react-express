29c51582cc7f9272b56cdc9297f7db6e0d5f6d00
Fyodor Simonov <fdr.smnv@outlook.com>
2021-12-23 12:35:53 +0300

    Create routes for feed and post pages

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -2,11 +2,18 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
-import App from "./components/App";
 import LoginPage from "./components/Login";
+import { Layout } from "./components/Layout";
 import { RequireLogin } from "./components/RequireLogin";
-import { BrowserRouter, Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { getSessionData } from "./redux/slice/user";
+import { Feed } from "./components/Feed";
+import {PostPage} from "./components/PostPage";
 
 store.dispatch(getSessionData());
 
@@ -14,24 +21,8 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <RequireLogin>
-              <App>
-                <Home />
-              </App>
-            </RequireLogin>
-          }
-        />
-        <Route
-          path="about"
-          element={
-            <RequireLogin>
-              <About />
-            </RequireLogin>
-          }
-        />
+        <Route path="/" element={<RequireLogin><Layout><Feed/></Layout></RequireLogin>} />
+        <Route path='/post/:postId' element={<RequireLogin><Layout><PostPage /></Layout></RequireLogin>}/>
         <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
@@ -39,32 +30,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// App.js
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
-  );
-}
