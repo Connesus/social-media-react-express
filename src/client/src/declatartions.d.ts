7913d8355bbc6d6b68cb829b7ff6d6f662c73039
Fyodor Simonov <fdr.smnv@outlook.com>
2022-01-03 23:41:15 +0300

    File declarations to fix ts issues

--- src/client/src/declatartions.d.ts
+++ src/client/src/declatartions.d.ts
@@ -1,4 +1,9 @@
-// declare module '*.css';
-// declare module "*.module.css";
-// declare module "*.module.scss";
+declare module '*.css';
+declare module "*.module.css";
+declare module "*.module.scss";
 declare module '*.scss';
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.gif";
