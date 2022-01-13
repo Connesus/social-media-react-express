7743b2f165f4ddaf5bdce426cb24d1d94d24eea7
Fyodor Simonov <fdr.smnv@outlook.com>
2022-01-13 15:17:43 +0300

    Id parser helper function

--- src/server/src/utils/helpers.ts
+++ src/server/src/utils/helpers.ts
@@ -1,8 +1,8 @@
 
-import { ObjectId } from 'mongoose';
 import { IUser } from './../model/user';
 import {SessionUserT} from "@shared/types";
 import {ErrorRequestHandler, RequestHandler} from "express";
+import mongoose from "mongoose";
 
 export const sessionizeUser: (user: IUser) => SessionUserT = ({ id, username }) => ({ id, username });
 
@@ -10,5 +10,8 @@ export const use = (fn: RequestHandler): RequestHandler => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next)
 
 export const errorHandler: ErrorRequestHandler = function (err, req, res, next) {
+  console.error(err.stack)
   return res.status(500).json({error: true, message: err.message})
 }
+
+export const parseIdStr = (id: unknown) => id ? new mongoose.Types.ObjectId(String(id)) : undefined;
