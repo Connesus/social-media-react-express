00ed925d8d7bee34e5db59f0eeecf3f65463a96d
Fyodor Simonov <fdr.smnv@outlook.com>
2022-01-13 15:17:23 +0300

    Init pagination plugin

---
+++ src/server/src/utils/paginationPlugin.ts
@@ -0,0 +1,36 @@
+import {FilterQuery, Model, Schema, SchemaOptions, SortValues} from "mongoose";
+
+export type TPaginationOptions = {
+  filter: FilterQuery<any>;
+  orderBy?: string;
+  sort: string | any;
+  limit?: number;
+  anchor?: any;
+}
+
+export interface pluginModel {
+  firstPage(options: TPaginationOptions): this
+  nextPage(options: TPaginationOptions): this
+  prevPage(options: TPaginationOptions): this
+}
+
+export default function paginationPlugin(schema: Schema, options: SchemaOptions) {
+  schema.statics.firstPage = function ({filter, orderBy = '_id', sort, limit = 10}: TPaginationOptions) {
+    return this
+      .find({...filter })
+      .sort()
+      .limit(limit)
+  }
+  schema.statics.nextPage = function ({filter, orderBy = '_id', sort, limit = 10, anchor}: TPaginationOptions) {
+    return this
+      .find({...filter, [orderBy]: { $lt: anchor} })
+      .sort(sort)
+      .limit(limit)
+  }
+  schema.statics.prevPage = function ({filter, orderBy = '_id', sort, limit = 10, anchor}: TPaginationOptions) {
+    return this
+      .find({...filter, [orderBy]: { $gt: anchor} })
+      .sort(sort)
+      .limit(limit)
+  }
+}
