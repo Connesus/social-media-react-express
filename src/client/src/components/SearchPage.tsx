8cc215620f7a2e6d51cde85238028b4cbb69cd86
Fyodor Simonov <fdr.smnv@outlook.com>
2022-01-16 00:00:51 +0300

    Init search page

---
+++ src/client/src/components/SearchPage.tsx
@@ -0,0 +1,29 @@
+import React, {useCallback, useRef} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../redux/store";
+import {searchPosts} from "../redux/slice/posts";
+import {Post} from "./Post";
+
+export const SearchPage: React.FC = () => {
+  const searchIds = useSelector((state: RootState) => state.post.searchIds);
+  const postTextRef = useRef<HTMLTextAreaElement>(null);
+  const dispatch = useDispatch();
+
+  const handleSearchButton = useCallback(() => {
+    if (postTextRef?.current?.value) {
+      dispatch(searchPosts(postTextRef.current.value))
+    }
+  }, [postTextRef])
+
+  return (
+    <div>
+      <div>
+        <textarea ref={postTextRef} placeholder='Enter post text'></textarea>
+        <button onClick={handleSearchButton}>Search</button>
+      </div>
+      <div>
+        {searchIds && searchIds.map(id => <Post id={id} />)}
+      </div>
+    </div>
+  )
+}
