baf812ff86c912c2fc38f7b89d0b7b6c6cfed0b8
Fyodor Simonov <fdr.smnv@outlook.com>
2022-01-18 22:41:50 +0300

    Update sidebar with icons

--- src/client/src/components/SideNavbar.tsx
+++ src/client/src/components/SideNavbar.tsx
@@ -4,21 +4,36 @@ import style from "../styles/SideNavbar.module.scss";
 import {Avatar} from "./Post";
 import {useSelector} from "react-redux";
 import {RootState} from "../redux/store";
+import {HomeIcon} from "./icons/HomeIcon";
+import {SearchIcon} from "./icons/SearchIcon";
+import {MessageIcon} from "./icons/MessageIcon";
+import {PostIcon} from "./icons/PostIcon";
+
+const navLinkClass = ({isActive}: {isActive: boolean}): string => [style.SideNavbar__link,
+  ...(isActive ? [style['SideNavbar__link--active']] : [])].join(' ');
 
 export const SideNavbar: React.FC = () => {
   const authUserData = useSelector((state:RootState) => state.auth)
   return (
     <nav className={style.SideNavbar}>
-      {authUserData.username ? (<NavLink to="/profile" className={style.SideNavbar__link}>
+      {authUserData.username ? (<NavLink to={'/user/' + authUserData.username} className={navLinkClass}>
           <Avatar username={authUserData.username} width={'64px'} height={'64px'} />
           <br/>
         {authUserData.username}
-      </NavLink>) : (<NavLink to={'/login'} className={style.SideNavbar__link} style={{color: 'blue'}}>Login</NavLink>)
+      </NavLink>) : (<NavLink to={'/login'} className={navLinkClass} style={{color: 'blue'}}>Login</NavLink>)
       }
-      <NavLink to="/" className={style.SideNavbar__link}>Feed</NavLink>
-      <NavLink to="/search" className={style.SideNavbar__link}>Search</NavLink>
-      <NavLink to="/create" className={style.SideNavbar__link}>Create New</NavLink>
-      <NavLink to="/messages" className={style.SideNavbar__link}>Messages</NavLink>
+      <NavLink to="/" className={navLinkClass}>
+        <HomeIcon width='48px' height='48px' />
+      </NavLink>
+      <NavLink to="/search" className={navLinkClass}>
+        <SearchIcon width='48px' height='48px' />
+      </NavLink>
+      <NavLink to="/create" className={navLinkClass}>
+        <PostIcon width='48px' height='48px' />
+      </NavLink>
+      <NavLink to="/messages" className={navLinkClass} >
+        <MessageIcon width='48px' height='48px' />
+      </NavLink>
     </nav>
   );
 };
