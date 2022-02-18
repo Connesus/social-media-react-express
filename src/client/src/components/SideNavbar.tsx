ffcae95511a63a81642f63770ddbb5aa72549a74
Fyodor Simonov <fdr.smnv@outlook.com>
2022-02-18 18:10:14 +0300

    Update navbar

--- src/client/src/components/SideNavbar.tsx
+++ src/client/src/components/SideNavbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import {matchRoutes, NavLink, PathMatch, useLocation, useMatch} from "react-router-dom";
 import style from "../styles/SideNavbar.module.scss";
 import {Avatar} from "./Post";
 import {useSelector} from "react-redux";
@@ -12,27 +12,50 @@ import {PostIcon} from "./icons/PostIcon";
 const navLinkClass = ({isActive}: {isActive: boolean}): string => [style.SideNavbar__link,
   ...(isActive ? [style['SideNavbar__link--active']] : [])].join(' ');
 
+const iconClassFn = (match: PathMatch | null) => {
+  console.log('match',!!match)
+  console.log('')
+  const res = [
+    style['SideNavbar__link-icon'],
+    ...(!!match ? [style['SideNavbar__link-icon--active']] : [])
+  ].join(' ');
+  console.log(res)
+  return res
+}
+
 export const SideNavbar: React.FC = () => {
   const authUserData = useSelector((state:RootState) => state.auth)
+  const location = useLocation()
   return (
     <nav className={style.SideNavbar}>
       {authUserData.username ? (<NavLink to={'/user/' + authUserData.username} className={navLinkClass}>
-          <Avatar username={authUserData.username} width={'64px'} height={'64px'} />
-          <br/>
-        {authUserData.username}
-      </NavLink>) : (<NavLink to={'/login'} className={navLinkClass} style={{color: 'blue'}}>Login</NavLink>)
+          <Avatar username={authUserData.username} inlineStyle={{width: 'var(--step-3)', height: 'var(--step-3)'}} />
+        <span className={style['SideNavbar__link-text']}>profile</span>
+      </NavLink>) : (<NavLink to={'/login'} className={navLinkClass}>LOGIN</NavLink>)
       }
       <NavLink to="/" className={navLinkClass}>
-        <HomeIcon className={style['SideNavbar__link-icon']} />
+        <div className={style['SideNavbar__link-container-icon']}>
+          <HomeIcon className={iconClassFn(useMatch('/'))} />
+        </div>
+        <span className={style['SideNavbar__link-text']}>feed</span>
       </NavLink>
       <NavLink to="/search" className={navLinkClass}>
-        <SearchIcon className={style['SideNavbar__link-icon']} />
+        <div className={style['SideNavbar__link-container-icon']}>
+          <SearchIcon className={iconClassFn(useMatch('/search'))} />
+        </div>
+        <span className={style['SideNavbar__link-text']}>search</span>
       </NavLink>
       <NavLink to="/create" className={navLinkClass}>
-        <PostIcon className={style['SideNavbar__link-icon']} />
+        <div className={style['SideNavbar__link-container-icon']}>
+          <PostIcon className={iconClassFn(useMatch('/create'))} />
+        </div>
+        <span className={style['SideNavbar__link-text']}>post</span>
       </NavLink>
       <NavLink to="/messages" className={navLinkClass} >
-        <MessageIcon className={style['SideNavbar__link-icon']} />
+        <div className={style['SideNavbar__link-container-icon']}>
+          <MessageIcon className={iconClassFn(useMatch('/messages'))} />
+        </div>
+        <span className={style['SideNavbar__link-text']}>message</span>
       </NavLink>
     </nav>
   );
